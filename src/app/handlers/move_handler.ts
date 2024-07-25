e28bcf58a6f7e5f203d13f410ded6c0c614f4eb1
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-25 19:54:23 -0300

    chore(libs): add lint and formatter libs

--- src/app/handlers/move_handler.ts
+++ src/app/handlers/move_handler.ts
@@ -1,18 +1,17 @@
-import { Request, Response } from "express"
+import { Request, Response } from 'express'
 
 export function moveHandler(req: Request, res: Response) {
   try {
     res.json({
-      apiversion: "1",
-      author: "Maua-Dev",
-      color: "#8B0000",
-      head: "tiger-king",
-      tail: "hook",
-      version: "1.0.0"
+      apiversion: '1',
+      author: 'Enzo Sakamoto',
+      color: '#8B0000',
+      head: 'tiger-king',
+      tail: 'hook',
+      version: '1.0.0'
     })
-
-  } catch (error: any) {
+  } catch (error) {
     console.error(error)
-    res.status(500).send("Internal Server Error: " + error.message)
+    res.status(500).send('Internal Server Error: ' + (error as Error).message)
   }
 }
