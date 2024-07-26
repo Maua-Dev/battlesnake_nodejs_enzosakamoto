ef0fee51e63c365c07d2d78222db3346bacada2f
Enzo Sakamoto <enzo.sak@hotmail.com>
2024-07-25 23:10:44 -0300

    fix: remove move endpoint

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,7 +3,7 @@ config()
 import express, { Request, Response } from 'express'
 import ServerlessHttp from 'serverless-http'
 import { STAGE } from './enums/stage_enum'
-import { router } from './routes/snake_routes'
+import { router } from './routes/snake-routes'
 
 const app = express()
 app.use(express.json())
@@ -11,17 +11,6 @@ app.use(router)
 
 app.post('/start', (req: Request, res: Response) => {
   res.send('ok')
-})
-
-app.post('/move', (req: Request, res: Response) => {
-  console.log(req.body)
-  const directions = ['up', 'down', 'left', 'right']
-  const i = Math.floor(Math.random() * directions.length)
-  const response = {
-    move: directions[i],
-    shout: `I'm moving ${directions[i]}!`
-  }
-  res.json(response)
 })
 
 app.post('/end', (req: Request, res: Response) => {
